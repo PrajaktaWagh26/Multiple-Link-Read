4e21a3341f250888f18f3dc7ecc003b06333ca41
PrajaktaWagh26 <wagh.prajakta@outlook.com>
2020-07-21 01:08:45 +0530

    Code for reading file

--- Dhiraj_Project/src/main/java/multiple_Links/Dhiraj_Project/ReadFile.java
+++ Dhiraj_Project/src/main/java/multiple_Links/Dhiraj_Project/ReadFile.java
@@ -1,5 +1,10 @@
 package multiple_Links.Dhiraj_Project;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReadFile {
@@ -8,9 +13,22 @@ public class ReadFile {
 	public ReadFile(String fileName) {
 		this.fileName = fileName;
 	}
-	
-	public List<String> readFileData(){
-		return null;
-		
+
+	public List<String> readFileData() throws IOException {
+		BufferedReader bufferReader = new BufferedReader(new FileReader(fileName));
+		List<String> fileData = new ArrayList<String>();
+		try {
+			String link = "";
+			while ((link = bufferReader.readLine()) != null) {
+				fileData.add(link);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			bufferReader.close();
+		}
+		return fileData;
 	}
 }
